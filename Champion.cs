9ab58551d4972cf852a102a4eb7cc0abfbea0d2c
joshc <elias242_@hotmail.com>
2017-05-20 13:12:57 -0700

    Initial Commit
    Created Champion class and accompanying unit tests

---
+++ Champion.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+namespace ChampAdvisor
+{
+    public class Champion
+    {
+        int _ImpactRating;
+        string[] Info;
+
+        public Champion(string[] championInfo)
+        {
+            if (championInfo.Length == 12)
+            {
+                Info = championInfo;
+            }
+            else
+            {
+                Debug.Write("System Exception - Champion data array must contain 12 elements");
+                throw new System.Exception("Champion data array must contain 12 elements");
+            }
+        }
+
+        public int ImpactRating { get => _ImpactRating; set => _ImpactRating = value; }
+    }
+}
+/*
+ Champion Data must contain the following
+ Name
+ Score
+ Aspects (Initiation, Engage, Haste, Sustain, Diver, Tank, Disengage)
+ Ideal ally comps
+ Ideal lane partner
+ Ideal enemy laner
+ Ideal enemy comp
+ Ideal enemy aspects (Spike Damage, Multiple Divers, Tanks, AD Heavy)
+ Unfavorable lane opponent
+ Unfavorable enemy champions
+ Unfavorable enemy comp
+ Unfavorable enemy aspects
+*/
